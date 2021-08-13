89b51447f2a6f1cd4c0331ebc80650cdb9a5aa54
rudietuesdays <rae@throneless.tech>
2021-08-13 12:12:27 -0400

    building articles model and styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  parser: "@babel/eslint-parser",
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true
@@ -15,9 +11,6 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module'
   },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
+  plugins: ['react'],
+  rules: {}
 }
