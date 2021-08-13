e3ee7d98658b817ad6ed5c682b935413d8e2f924
rudietuesdays <rae@throneless.tech>
2021-08-13 12:13:34 -0400

    add and config eslint

--- sourcebit.js
+++ sourcebit.js
@@ -1,63 +1,62 @@
-const _ = require('lodash');
+const _ = require('lodash')
 
-const isDev = process.env.NODE_ENV === 'development';
-
+const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
   plugins: [
     {
-      module: require("sourcebit-source-sanity"),
+      module: require('sourcebit-source-sanity'),
       options: {
         accessToken: process.env.SANITY_ACCESS_TOKEN,
-        projectId: process.env.SANITY_PROJECT_ID || "3tzzh18d",
-        dataset: process.env.SANITY_DATASET || "production",
+        projectId: process.env.SANITY_PROJECT_ID || '3tzzh18d',
+        dataset: process.env.SANITY_DATASET || 'production',
         isPreview: isDev,
-        watch: isDev,
-      },
+        watch: isDev
+      }
     },
     {
-      module: require("sourcebit-target-next"),
+      module: require('sourcebit-target-next'),
       options: {
         liveUpdate: isDev,
         flattenAssetUrls: true,
         pages: [
           {
-            path: "/{stackbit_url_path}",
-            predicate: _.matchesProperty("__metadata.modelName", "advanced"),
+            path: '/{stackbit_url_path}',
+            predicate: _.matchesProperty('__metadata.modelName', 'advanced')
           },
           {
-            path: "/{stackbit_url_path}",
-            predicate: _.matchesProperty("__metadata.modelName", "article"),
+            path: '/{stackbit_url_path}',
+            predicate: _.matchesProperty('__metadata.modelName', 'article')
           },
           {
-            path: "/{stackbit_url_path}",
-            predicate: _.matchesProperty("__metadata.modelName", "page"),
+            path: '/{stackbit_url_path}',
+            predicate: _.matchesProperty('__metadata.modelName', 'page')
           },
           {
-            path: "/{stackbit_url_path}",
-            predicate: _.matchesProperty("__metadata.modelName", "post"),
+            path: '/{stackbit_url_path}',
+            predicate: _.matchesProperty('__metadata.modelName', 'post')
           },
           {
-            path: "/{stackbit_url_path}",
-            predicate: _.matchesProperty("__metadata.modelName", "syllabus"),
-          },
+            path: '/{stackbit_url_path}',
+            predicate: _.matchesProperty('__metadata.modelName', 'syllabus')
+          }
         ],
-        commonProps: (items) => {
+        commonProps: items => {
           return {
-            pages: _.filter(items, (item) =>
-              ["advanced", "article", "page", "post", "syllabus"].includes(
-                _.get(item, "__metadata.modelName")
+            pages: _.filter(items, item =>
+              ['advanced', 'article', 'page', 'post', 'syllabus'].includes(
+                _.get(item, '__metadata.modelName')
               )
             ),
             data: {
               config: _.find(
                 items,
-                _.matchesProperty("__metadata.modelName", "config")
-              ),
-            },
-          };
-        },
-      },
-    },
-  ],
-};
+                _.matchesProperty('__metadata.modelName', 'config')
+              )
+            }
+          }
+        }
+      }
+    }
+  ]
+}
