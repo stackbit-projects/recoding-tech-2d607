09e50a75a8e9c5136af39f26b549d978bf894e1f
rudietuesdays <rae@throneless.tech>
2021-08-20 11:12:36 -0400

    begin to implement material ui theme provider customization

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import ScriptTag from 'react-script-tag';
-import { withPrefix } from '../utils';
-
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import ScriptTag from 'react-script-tag'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { withPrefix } from '../utils'
+import theme from '../theme.js'
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
 
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps }
-    }
-
-    render() {
-        return (
-            <Html>
-                <Head />
-                <body>
-                    <Main />
-                    <ScriptTag src={withPrefix('js/init.js')}/>
-                    <ScriptTag src={withPrefix('js/page-load.js')}/>
-                    <ScriptTag src={withPrefix('js/page-unload.js')}/>
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <ThemeProvider theme={theme}>
+            <Main />
+          </ThemeProvider>
+          <ScriptTag src={withPrefix('js/init.js')} />
+          <ScriptTag src={withPrefix('js/page-load.js')} />
+          <ScriptTag src={withPrefix('js/page-unload.js')} />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
-export default MyDocument;
+export default MyDocument
