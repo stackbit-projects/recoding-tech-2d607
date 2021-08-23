0d8a641714b87f738a2e14d346aa0f1dd9090df6
Harum Helmy <harumendhah@gmail.com>
2021-08-23 13:56:21 -0400

    Starting to implement styles to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,76 +4,242 @@ import _ from 'lodash';
 import {Link, withPrefix, classNames} from '../utils';
 import Icon from './Icon';
 
-export default class Header extends React.Component {
+// material ui imports
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  header: {
+    background: theme.palette.primary,
+  },
+}));
+
+class Header extends React.Component {
     render() {
+
+      const { classes } = this.props
+
         return (
-            <header id="masthead" className={'site-header ' + _.get(this.props, 'data.config.header.background', null)}>
-              <div className="site-header-wrap">
-                <div className="site-header-inside">
-                  <div className="site-branding">
-                    {_.get(this.props, 'data.config.header.profile_img', null) && (
+          <header
+            id="masthead"
+            className={classes.header}
+          >
+            <Box m={4}>
+              <Grid container spacing={1} justifyContent="space-between">
+                <Grid item>
+                  <Typography>recoding.tech</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography>Navigation baby</Typography>
+                </Grid>
+              </Grid>
+            </Box>
+            <div className="site-header-wrap">
+              <div className="site-header-inside">
+                <div className="site-branding">
+                  {_.get(
+                    this.props,
+                    "data.config.header.profile_img",
+                    null
+                  ) && (
                     <p className="profile">
-                      <Link href={withPrefix('/')}><img src={withPrefix(_.get(this.props, 'data.config.header.profile_img', null))}
-                          className="avatar" alt={_.get(this.props, 'data.config.header.profile_img_alt', null)} /></Link>
+                      <Link href={withPrefix("/")}>
+                        <img
+                          src={withPrefix(
+                            _.get(
+                              this.props,
+                              "data.config.header.profile_img",
+                              null
+                            )
+                          )}
+                          className="avatar"
+                          alt={_.get(
+                            this.props,
+                            "data.config.header.profile_img_alt",
+                            null
+                          )}
+                        />
+                      </Link>
                     </p>
-                    )}
-                    <div className="site-identity">
-                      <p className="site-title"><Link href={withPrefix('/')}>{_.get(this.props, 'data.config.header.title', null)}</Link></p>
-                      {_.get(this.props, 'data.config.header.tagline', null) && (
-                      <p className="site-description">{_.get(this.props, 'data.config.header.tagline', null)}</p>
-                      )}
-                    </div>
-                    {(_.get(this.props, 'data.config.header.has_nav', null) || _.get(this.props, 'data.config.header.has_social', null)) && (
-                    <button id="menu-toggle" className="menu-toggle"><span className="screen-reader-text">Menu</span><span className="icon-menu"
-                        aria-hidden="true" /></button>
+                  )}
+                  <div className="site-identity">
+                    <p className="site-title">
+                      <Link href={withPrefix("/")}>
+                        {_.get(this.props, "data.config.header.title", null)}
+                      </Link>
+                    </p>
+                    {_.get(this.props, "data.config.header.tagline", null) && (
+                      <p className="site-description">
+                        {_.get(this.props, "data.config.header.tagline", null)}
+                      </p>
                     )}
                   </div>
-                  {(_.get(this.props, 'data.config.header.has_nav', null) || _.get(this.props, 'data.config.header.has_social', null)) && (
-                  <nav id="main-navigation" className="site-navigation" aria-label="Main Navigation">
+                  {(_.get(this.props, "data.config.header.has_nav", null) ||
+                    _.get(
+                      this.props,
+                      "data.config.header.has_social",
+                      null
+                    )) && (
+                    <button id="menu-toggle" className="menu-toggle">
+                      <span className="screen-reader-text">Menu</span>
+                      <span className="icon-menu" aria-hidden="true" />
+                    </button>
+                  )}
+                </div>
+                {(_.get(this.props, "data.config.header.has_nav", null) ||
+                  _.get(this.props, "data.config.header.has_social", null)) && (
+                  <nav
+                    id="main-navigation"
+                    className="site-navigation"
+                    aria-label="Main Navigation"
+                  >
                     <div className="site-nav-wrap">
                       <div className="site-nav-inside">
-                        {_.get(this.props, 'data.config.header.has_nav', null) && (
-                        <ul className="menu">
-                          {_.map(_.get(this.props, 'data.config.header.nav_links', null), (action, action_idx) => {
-                              let pageUrl = _.trim(_.get(this.props, 'page.stackbit_url_path', null), '/');
-                              let actionUrl = _.trim(_.get(action, 'url', null), '/');
-                              return (
-                                <li key={action_idx} className={classNames('menu-item', {'current-menu-item': pageUrl === actionUrl, 'menu-button': _.get(action, 'style', null) === 'button'})}>
-                                  <Link href={withPrefix(_.get(action, 'url', null))}
-                                    {...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
-                                    {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
-                                    className={classNames({'button': _.get(action, 'style', null) === 'button'})}>{_.get(action, 'label', null)}</Link>
-                                </li>
-                              )
-                          })}
-                        </ul>
+                        {_.get(
+                          this.props,
+                          "data.config.header.has_nav",
+                          null
+                        ) && (
+                          <ul className="menu">
+                            {_.map(
+                              _.get(
+                                this.props,
+                                "data.config.header.nav_links",
+                                null
+                              ),
+                              (action, action_idx) => {
+                                let pageUrl = _.trim(
+                                  _.get(
+                                    this.props,
+                                    "page.stackbit_url_path",
+                                    null
+                                  ),
+                                  "/"
+                                );
+                                let actionUrl = _.trim(
+                                  _.get(action, "url", null),
+                                  "/"
+                                );
+                                return (
+                                  <li
+                                    key={action_idx}
+                                    className={classNames("menu-item", {
+                                      "current-menu-item":
+                                        pageUrl === actionUrl,
+                                      "menu-button":
+                                        _.get(action, "style", null) ===
+                                        "button",
+                                    })}
+                                  >
+                                    <Link
+                                      href={withPrefix(
+                                        _.get(action, "url", null)
+                                      )}
+                                      {...(_.get(action, "new_window", null)
+                                        ? { target: "_blank" }
+                                        : null)}
+                                      {...(_.get(action, "new_window", null) ||
+                                      _.get(action, "no_follow", null)
+                                        ? {
+                                            rel:
+                                              (_.get(action, "new_window", null)
+                                                ? "noopener "
+                                                : "") +
+                                              (_.get(action, "no_follow", null)
+                                                ? "nofollow"
+                                                : ""),
+                                          }
+                                        : null)}
+                                      className={classNames({
+                                        button:
+                                          _.get(action, "style", null) ===
+                                          "button",
+                                      })}
+                                    >
+                                      {_.get(action, "label", null)}
+                                    </Link>
+                                  </li>
+                                );
+                              }
+                            )}
+                          </ul>
                         )}
-                        {_.get(this.props, 'data.config.header.has_social', null) && false && (
-                        <div className="social-links">
-                          {_.map(_.get(this.props, 'data.config.header.social_links', null), (action, action_idx) => (
-                          action && (
-                          <Link key={action_idx} href={withPrefix(_.get(action, 'url', null))}
-                            {...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
-                            {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
-                            className={classNames({'button button-icon': _.get(action, 'style', null) === 'icon'})}>
-                            {((_.get(action, 'style', null) === 'icon') && _.get(action, 'icon_class', null)) ? (<React.Fragment>
-                              <Icon {...this.props} icon={_.get(action, 'icon_class', null)} />
-                              <span className="screen-reader-text">{_.get(action, 'label', null)}</span>
-                            </React.Fragment>) : 
-                            _.get(action, 'label', null)
-                            }
-                          </Link>
-                          )
-                          ))}
-                        </div>
-                        )}
+                        {_.get(
+                          this.props,
+                          "data.config.header.has_social",
+                          null
+                        ) &&
+                          false && (
+                            <div className="social-links">
+                              {_.map(
+                                _.get(
+                                  this.props,
+                                  "data.config.header.social_links",
+                                  null
+                                ),
+                                (action, action_idx) =>
+                                  action && (
+                                    <Link
+                                      key={action_idx}
+                                      href={withPrefix(
+                                        _.get(action, "url", null)
+                                      )}
+                                      {...(_.get(action, "new_window", null)
+                                        ? { target: "_blank" }
+                                        : null)}
+                                      {...(_.get(action, "new_window", null) ||
+                                      _.get(action, "no_follow", null)
+                                        ? {
+                                            rel:
+                                              (_.get(action, "new_window", null)
+                                                ? "noopener "
+                                                : "") +
+                                              (_.get(action, "no_follow", null)
+                                                ? "nofollow"
+                                                : ""),
+                                          }
+                                        : null)}
+                                      className={classNames({
+                                        "button button-icon":
+                                          _.get(action, "style", null) ===
+                                          "icon",
+                                      })}
+                                    >
+                                      {_.get(action, "style", null) ===
+                                        "icon" &&
+                                      _.get(action, "icon_class", null) ? (
+                                        <React.Fragment>
+                                          <Icon
+                                            {...this.props}
+                                            icon={_.get(
+                                              action,
+                                              "icon_class",
+                                              null
+                                            )}
+                                          />
+                                          <span className="screen-reader-text">
+                                            {_.get(action, "label", null)}
+                                          </span>
+                                        </React.Fragment>
+                                      ) : (
+                                        _.get(action, "label", null)
+                                      )}
+                                    </Link>
+                                  )
+                              )}
+                            </div>
+                          )}
                       </div>
                     </div>
                   </nav>
-                  )}
-                </div>
+                )}
               </div>
-            </header>
+            </div>
+          </header>
         );
     }
 }
+
+export default withStyles(useStyles)(Header);
