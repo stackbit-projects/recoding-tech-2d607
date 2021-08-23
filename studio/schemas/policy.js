b04a3b50ceeb37141263d0592a15a0518599a063
Harum Helmy <harumendhah@gmail.com>
2021-08-23 13:08:11 -0400

    New things on the topic model

--- studio/schemas/policy.js
+++ studio/schemas/policy.js
@@ -1,117 +1,117 @@
 export default {
-  type: 'document',
-  name: 'policy',
-  title: 'Policy',
+  type: "document",
+  name: "policy",
+  title: "Policy",
   fields: [
     {
-      name: 'title',
-      type: 'string',
-      title: 'Policy name',
-      description: 'Official name of the policy',
-      validation: Rule => Rule.required()
+      name: "title",
+      type: "string",
+      title: "Policy name",
+      description: "Official name of the policy",
+      validation: (Rule) => Rule.required(),
     },
     {
-      title: 'Slug',
-      name: 'slug',
-      type: 'slug',
+      title: "Slug",
+      name: "slug",
+      type: "slug",
       description:
-        'The slug for the policy. Can be the same as the name, but turned into a URL. For example, name-of-policy.',
-      validation: Rule => Rule.required(),
+        "The slug for the policy. Can be the same as the name, but turned into a URL. For example, name-of-policy.",
+      validation: (Rule) => Rule.required(),
       options: {
-        source: 'title',
+        source: "title",
         maxLength: 200, // will be ignored if slugify is set
-        slugify: input =>
+        slugify: (input) =>
           input
             .toLowerCase()
-            .replace(/[^\w\s]/gi, '')
-            .replace(/\s+/g, '-')
-            .slice(0, 200)
-      }
+            .replace(/[^\w\s]/gi, "")
+            .replace(/\s+/g, "-")
+            .slice(0, 200),
+      },
     },
     {
-      name: 'type',
-      type: 'string',
-      title: 'Policy type',
-      description: 'E.g. legislation, proposal, etc.',
-      validation: Rule => Rule.required()
+      name: "type",
+      type: "string",
+      title: "Policy type",
+      description: "E.g. legislation, proposal, etc.",
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: 'country',
-      type: 'string',
-      title: 'Country',
+      name: "country",
+      type: "string",
+      title: "Country",
       description: "The policy's country of origin",
-      validation: Rule => Rule.required()
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: 'dateInitiated',
-      title: 'Date initiated',
-      type: 'date',
+      name: "dateInitiated",
+      title: "Date initiated",
+      type: "date",
       options: {
-        dateFormat: 'MMMM DD YYYY'
-      }
+        dateFormat: "MMMM DD YYYY",
+      },
     },
     {
-      name: 'status',
-      title: 'Current status of policy',
-      type: 'string',
-      description: 'E.g. passed by committee, enacted, etc.'
+      name: "status",
+      title: "Current status of policy",
+      type: "string",
+      description: "E.g. passed by committee, enacted, etc.",
     },
     {
-      name: 'lastUpdate',
-      title: 'Date of last update',
-      type: 'date',
+      name: "lastUpdate",
+      title: "Date of last update",
+      type: "date",
       options: {
-        dateFormat: 'MMMM DD YYYY'
-      }
+        dateFormat: "MMMM DD YYYY",
+      },
     },
     {
-      type: 'markdown',
-      name: 'summary',
-      title: 'Summary',
-      description: 'A summary of the policy.',
-      validation: Rule => Rule.required()
+      type: "markdown",
+      name: "summary",
+      title: "Summary",
+      description: "A summary of the policy.",
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: 'relatedCitations',
-      title: 'Further reading',
-      type: 'array',
+      name: "relatedCitations",
+      title: "Further reading",
+      type: "array",
       of: [
         {
-          type: 'reference',
+          type: "reference",
           to: [
             {
-              type: 'citation'
-            }
-          ]
-        }
-      ]
+              type: "citation",
+            },
+          ],
+        },
+      ],
     },
     {
-      name: 'relatedDocs',
-      title: 'Related Primary Documents',
-      type: 'array',
-      of: [{ type: 'file' }]
+      name: "relatedDocs",
+      title: "Related Primary Documents",
+      type: "array",
+      of: [{ type: "file" }],
     },
     {
-      name: 'relatedTopics',
-      title: 'Related Topics',
-      type: 'array',
+      name: "relatedTopics",
+      title: "Related Topics",
+      type: "array",
       of: [
         {
-          type: 'reference',
-          to: [{ type: 'topic' }]
-        }
-      ]
+          type: "reference",
+          to: [{ type: "topic" }],
+        },
+      ],
     },
     {
-      type: 'string',
-      name: 'layout',
-      title: 'Layout',
+      type: "string",
+      name: "layout",
+      title: "Layout",
       hidden: false,
-      validation: Rule => Rule.required(),
+      validation: (Rule) => Rule.required(),
       options: {
-        list: ['policy']
-      }
-    }
-  ]
-}
+        list: ["policy"],
+      },
+    },
+  ],
+};
--- studio/schemas/topic.js
+++ studio/schemas/topic.js
@@ -1,19 +1,52 @@
 export default {
-  "type": "document",
-  "name": "topic",
-  "title": "Topic",
-  "fields": [
+  type: "document",
+  name: "topic",
+  title: "Topic",
+  fields: [
     {
-        "type": "string",
-        "name": "name",
-        "title": "Name",
-        "description": "The name of the topic",
-        "validation": Rule => Rule.required()
+      title: "Name",
+      type: "string",
+      name: "name",
+      description: "The name of the topic.",
+      validation: (Rule) => Rule.required(),
     },
+    {
+      title: "Display title",
+      type: "string",
+      name: "displayTitle",
+      description: "How you want the name of the topic to appear on the front-end",
+    },
+    {
+      title: "Type of topic",
+      type: "string",
+      name: "topicType",
+      options: {
+        list: [
+          { title: "Country", value: "country" },
+          { title: "Issue", value: "issue" },
+          { title: "Company", value: "company" },
+        ],
+      },
+    },
+    {
+      title: "Fast facts",
+      type: "markdown",
+      name: "fastFacts",
+      description: "Featured stats and facts about the topic."
+    },
+    {
+      title: "Topic description",
+      type: "markdown", 
+      name: "topicDescription",
+      description: "Anything you want to say about the topic."
+    },
+    {
+      title: "Related commentary and analysis",
+    }
   ],
-  "preview": {
-      "select": {
-          "title": "name"
-      }
-  }
-}
+  preview: {
+    select: {
+      title: "name",
+    },
+  },
+};
