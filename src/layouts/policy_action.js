4ccf1914f290c48f1ec123b68751e7bb0f80d9ec
Harum Helmy <harumendhah@gmail.com>
2021-12-29 11:49:49 -0500

    Added necessary components to policy action layout, fix some CSS on the RelatedDocuments component

--- src/layouts/policy_action.js
+++ src/layouts/policy_action.js
@@ -15,32 +15,43 @@ import { Layout } from "../components/index";
 import SectionHero from "../components/SectionHero";
 import TrackerActions from "../components/TrackerActions";
 import RelatedDocuments from "../components/RelatedDocuments";
+import RelatedCommentary from "../components/RelatedCommentary";
+import RelatedTopics from "../components/RelatedTopics";
+
 
 const PolicyAction = props => {
   const { page } = props;
-  console.log(page);
 
   return (
     <Layout {...props}>
       <SectionHero {...props} />
-      <Box my={4}>
+      <Box my={8}>
         <Container>
           <TrackerActions {...props} />
-          <Box my={4}>
-            <Grid container>
+          <Grid container spacing={8}>
+            <Grid container spacing={12} direction="column" item sm={12} md={8}>
               <Grid item sm={12} md={8}>
                 <Typography component="div" variant="body1">
                   {htmlToReact(page.summary)}
                 </Typography>
               </Grid>
-              <Grid item sm={12} md={4}>
-                <Typography component="div" variant="h4">
-                  Further reading
-                </Typography>
-                <RelatedDocuments {...props} />
+            </Grid>
+            <Grid container spacing={4} direction="column" item sm={12} md={4}s>
+              <Grid item>
+                <RelatedCommentary
+                  title={"Further reading"}
+                  page={page}
+                  commentary={page.relatedCitations}
+                />
+              </Grid>
+              <Grid item>
+                <RelatedTopics topics={page.relatedTopics}/>
+              </Grid>
+              <Grid item>
+                <RelatedDocuments {...props} /> 
               </Grid>
             </Grid>
-          </Box>
+          </Grid>
         </Container>
       </Box>
     </Layout>
