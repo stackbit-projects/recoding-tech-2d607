d58679febf5338aa3a775063dac1449472f9609f
Harum Helmy <harumendhah@gmail.com>
2022-01-06 15:43:29 -0500

    Add controller and styling to form

--- src/components/SectionForm.js
+++ src/components/SectionForm.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import _ from 'lodash';
-import { useForm } from "react-hook-form";
-import { useRouter } from "next/router";
+import React from "react";
+import _ from "lodash";
+import { useForm, Controller } from "react-hook-form";
 
-import {markdownify} from '../utils';
+import { markdownify } from "../utils";
 
 // Material UI import
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 
 export default function SectionForm(props) {
   let section = _.get(props, "section", null);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const { handleSubmit, control, reset } = useForm();
 
   // Transforms the form data from the React Hook Form output to a format Netlify can read
   const encode = (data) => {
@@ -28,20 +25,26 @@ export default function SectionForm(props) {
       .join("&");
   };
 
-  let onSubmit = (formData, event) => {
-    fetch(`/`, {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact-form", ...formData }),
-    })
-      .then((response) => {
-        router.push("/");
-      })
-      .catch((error) => {
-        alert("Failed to submit contact form, please try again.", error);
-      });
-    event.preventDefault();
-  };
+  // const onSubmit = (formData, event) => {
+  //   fetch(`/`, {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //     body: encode({ "form-name": "contact-form", ...formData }),
+  //   })
+  //     .then((response) => {
+  //       console.log("response", response);
+  //     })
+  //     .catch((error) => {
+  //       alert("Failed to submit contact form, please try again.", error);
+  //     });
+  //   event.preventDefault();
+  // };
+
+  const onSubmit = (formData, event) => {
+    console.log("formData", formData)
+    console.log("event", event)
+    reset();
+  }
 
   return (
     <section
@@ -65,6 +68,7 @@ export default function SectionForm(props) {
           method="POST"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit(onSubmit)}
         >
           <div className="screen-reader-text">
             <label>
@@ -77,13 +81,75 @@ export default function SectionForm(props) {
             name="form-name"
             value={_.get(section, "form_id", null)}
           />
-          <label htmlFor="name">
-            <Typography component="div" variant="subtitle1">
-              Name:
-            </Typography>
-            <input name="name" type="text" {...register("name")} />
-          </label>
-          <label htmlFor="email">
+            <Grid container spacing={8} item xs={12} md={11}>
+              <Grid item>
+                <Controller
+                  name="name"
+                  control={control}
+                  defaultValue=""
+                  render={({
+                    field: { onChange, value },
+                    fieldState: { error },
+                  }) => (
+                    <TextField
+                      label="Name"
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item>
+                <Controller
+                  name="email"
+                  control={control}
+                  defaultValue=""
+                  render={({
+                    field: { onChange, value },
+                    fieldState: { error },
+                  }) => (
+                    <TextField
+                      label="Email"
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                      helperText={error ? error.message : null}
+                    />
+                  )}
+                  rules={{
+                    required: "Email is required",
+                    pattern:
+                      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+                  }}
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} xs={12}>
+              <Grid item>
+                <Controller
+                  name="Message"
+                  control={control}
+                  defaultValue=""
+                  render={({
+                    field: { onChange, value },
+                    fieldState: { error },
+                  }) => (
+                    <TextField
+                      label="Message"
+                      fullWidth
+                      multiline
+                      rows={10}
+                      value={value}
+                      onChange={onChange}
+                      error={!!error}
+                      helperText={error ? error.message : null}
+                    />
+                  )}
+                />
+              </Grid>
+            </Grid>
+          {/* <label htmlFor="email">
             <Typography component="div" variant="subtitle1">
               Email:
             </Typography>
@@ -101,8 +167,8 @@ export default function SectionForm(props) {
                   /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
               })}
             />
-          </label>
-          <label htmlFor="message">
+          </label> */}
+          {/* <label htmlFor="message">
             <Typography component="div" variant="subtitle1">
               Message:
             </Typography>
@@ -111,7 +177,7 @@ export default function SectionForm(props) {
               rows={10}
               {...register("inquiry")}
             ></textarea>
-          </label>
+          </label> */}
           <div className="form-submit">
             <Button type="submit" className="button">
               {_.get(section, "submit_label", null)}
