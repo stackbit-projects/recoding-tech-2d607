9e2d8ce65b6663711997cee40d23a429a948e4b1
Harum Helmy <harumendhah@gmail.com>
2022-01-06 14:33:11 -0500

    Added styling to contact form

--- src/components/SectionForm.js
+++ src/components/SectionForm.js
@@ -1,73 +1,124 @@
 import React from 'react';
 import _ from 'lodash';
 import { useForm } from "react-hook-form";
-// import { useRouter } from "next/router";
+import { useRouter } from "next/router";
 
 import {markdownify} from '../utils';
-import FormField from './FormField';
 
+// Material UI import
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-// const {
-//   register,
-//   handleSubmit,
-//   formState: { errors },
-// } = useForm();
+export default function SectionForm(props) {
+  let section = _.get(props, "section", null);
 
-// const encode = (data) => {
-//   return Object.keys(data)
-//     .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-//     .join("&");
-// };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-const SectionForm = props => {
-  let section = _.get(props, 'section', null);
+  // Transforms the form data from the React Hook Form output to a format Netlify can read
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
+
+  let onSubmit = (formData, event) => {
+    fetch(`/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact-form", ...formData }),
+    })
+      .then((response) => {
+        router.push("/");
+      })
+      .catch((error) => {
+        alert("Failed to submit contact form, please try again.", error);
+      });
+    event.preventDefault();
+  };
+
   return (
-      <section
-        id={_.get(section, "section_id", null)}
-        className="block block-form"
-      >
+    <section
+      id={_.get(section, "section_id", null)}
+      className="block block-form"
+    >
+      <Box sx={{ border: 1, p: 5, bgcolor: "#EFE9DA" }}>
         {_.get(section, "title", null) && (
           <Typography component="div" variant="h2" className="html-to-react">
             {_.get(section, "title", null)}
           </Typography>
         )}
-        <div className="block-content inner-sm">
-          {_.get(section, "content", null) &&
-            markdownify(_.get(section, "content", null))}
-          <form
-            name={_.get(section, "form_id", null)}
-            id={_.get(section, "form_id", null)}
-            {...(_.get(section, "form_action", null)
-              ? { action: _.get(section, "form_action", null) }
-              : null)}
-            method="POST"
-            data-netlify="true"
-            data-netlify-honeypot="bot-field"
-          >
-            <div className="screen-reader-text">
-              <label>
-                {"Don't fill this out if you're human: "}
-                <input name="bot-field" />
-              </label>
-            </div>
+        {_.get(section, "content", null) &&
+          markdownify(_.get(section, "content", null))}
+        <form
+          name={_.get(section, "form_id", null)}
+          id={_.get(section, "form_id", null)}
+          {...(_.get(section, "form_action", null)
+            ? { action: _.get(section, "form_action", null) }
+            : null)}
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <div className="screen-reader-text">
+            <label>
+              {"Don't fill this out if you're human: "}
+              <input name="bot-field" />
+            </label>
+          </div>
+          <input
+            type="hidden"
+            name="form-name"
+            value={_.get(section, "form_id", null)}
+          />
+          <label htmlFor="name">
+            <Typography component="div" variant="subtitle1">
+              Name:
+            </Typography>
+            <input name="name" type="text" {...register("name")} />
+          </label>
+          <label htmlFor="email">
+            <Typography component="div" variant="subtitle1">
+              Email:
+            </Typography>
+            {errors.email && (
+              <span>
+                Please ensure your email address is formatted correctly.
+              </span>
+            )}
             <input
-              type="hidden"
-              name="form-name"
-              value={_.get(section, "form_id", null)}
+              type="text"
+              name="email"
+              {...register("email", {
+                required: true,
+                pattern:
+                  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+              })}
             />
-            {_.map(_.get(section, "form_fields", null), (field, field_idx) => (
-              <FormField key={field_idx} {...props} field={field} />
-            ))}
-            <div className="form-submit">
-              <button type="submit" className="button">
-                {_.get(section, "submit_label", null)}
-              </button>
-            </div>
-          </form>
-        </div>
-      </section>
+          </label>
+          <label htmlFor="message">
+            <Typography component="div" variant="subtitle1">
+              Message:
+            </Typography>
+            <textarea
+              name="message"
+              rows={10}
+              {...register("inquiry")}
+            ></textarea>
+          </label>
+          <div className="form-submit">
+            <Button type="submit" className="button">
+              {_.get(section, "submit_label", null)}
+            </Button>
+          </div>
+        </form>
+      </Box>
+    </section>
   );
 }
-
-export default SectionForm;
--- src/layouts/page.js
+++ src/layouts/page.js
@@ -2,7 +2,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import _ from "lodash";
-import components, { Layout } from "../components/index";
 import { markdownify } from "../utils";
 
 // Material UI imports
@@ -11,13 +10,15 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 
+import components, { Layout } from "../components/index";
+
 import SectionHero from "../components/SectionHero";
 import Sidebar from "../components/Sidebar";
 
-const Page = props => {
-  console.log("props in page", props)
+const Page = (props) => {
+  console.log("props in page", props);
   const {
-    page: { sidebar_content = {} }
+    page: { sidebar_content = {} },
   } = props;
 
   return (
@@ -38,29 +39,33 @@ const Page = props => {
                 </Typography>
               </div>
             </Grid>
+            <Grid item xs={12} sm={8}>
+              <div className="post-content inner-sm">
+                {_.map(
+                  _.get(props, "page.sections", null),
+                  (section, section_idx) => {
+                    let component = _.upperFirst(
+                      _.camelCase(_.get(section, "type", null))
+                    );
+                    let Component = components[component];
+                    return (
+                      <Component
+                        key={section_idx}
+                        {...props}
+                        section={section}
+                        site={props}
+                      />
+                    );
+                  }
+                )}
+              </div>
+            </Grid>
             <Grid item xs={12} sm={4}>
               {sidebar_content.length ? (
                 <Sidebar content={sidebar_content} />
               ) : null}
             </Grid>
           </Grid>
-          {_.map(
-            _.get(props, "page.sections", null),
-            (section, section_idx) => {
-              let component = _.upperFirst(
-                _.camelCase(_.get(section, "type", null))
-              );
-              let Component = components[component];
-              return (
-                <Component
-                  key={section_idx}
-                  {...props}
-                  section={section}
-                  site={props}
-                />
-              );
-            }
-          )}
         </Container>
       </Box>
     </Layout>
@@ -68,7 +73,7 @@ const Page = props => {
 };
 
 Page.propTypes = {
-  page: PropTypes.object
+  page: PropTypes.object,
 };
 
 export default Page;
