a87ed5dbb2eeea3964ad5437dfd6fc92e235cb81
Harum Helmy <harumendhah@gmail.com>
2022-01-27 14:35:57 -0500

    Update sanityClient configs on the submission-created function

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,9 +1,10 @@
 const sanityClient = require("@sanity/client");
 const client = sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  token: process.env.SANITY_ACCESS_TOKEN,
-  useCDN: false,
+  projectId: process.env.sanityProjectId || process.env.SANITY_PROJECT_ID,
+  dataset: process.env.sanityDataset || "production",
+  apiVersion: process.env.sanityApiVersion || "2021-03-25",
+  token: process.env.sanityAccessToken || process.env.SANITY_ACCESS_TOKEN,
+  useCdn: false,
 });
 
 exports.handler = async function (event, context, callback) {
