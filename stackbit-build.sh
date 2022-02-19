31df5cab5f0ecae5797e0c197bba020a121fc240
Josh King <jking@chambana.net>
2022-02-18 20:11:42 -0500

    More dependency updates.

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -8,13 +8,13 @@ echo "stackbit-build.sh: start build"
 
 # get the first commit hash and run studio-build.js, it will install and deploy sanity studio only if needed
 # to optimize the build time, studio-build.js runs in background in parallel to site build command
-#initialGitHash=$(git rev-list --max-parents=0 HEAD)
-#node ./studio-build.js $initialGitHash &
+initialGitHash=$(git rev-list --max-parents=0 HEAD)
+node ./studio-build.js $initialGitHash &
 
 # build site
 npm run build:netlify
 
 # wait for studio-build.js
-#wait
+wait
 
 echo "stackbit-build.sh: finished build"
