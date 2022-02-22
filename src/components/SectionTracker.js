55ca6bc494bb2c9dea910ce6dcca0d9f71415aff
Harum Helmy <harumendhah@gmail.com>
2022-02-22 14:38:08 -0500

    Fix linting error

--- src/components/SectionTracker.js
+++ src/components/SectionTracker.js
@@ -474,152 +474,151 @@ function SectionTracker(props) {
             .sort((a, b) => new Date(a.lastUpdate) - new Date(b.lastUpdate))
             .reverse()
             .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            .map((row) =>(
-                <Paper elevation={0} key={row._key} sx={{ marginBottom: 4 }}>
-                  <Grid container>
-                    <Grid
-                      item
-                      xs={12}
+            .map((row) => (
+              <Paper elevation={0} key={row._key} sx={{ marginBottom: 4 }}>
+                <Grid container>
+                  <Grid
+                    item
+                    xs={12}
+                    sx={{
+                      backgroundColor: "#EFE9DA",
+                      padding: 2,
+                      marginBottom: 2,
+                    }}
+                  >
+                    <Link
+                      href={`/tracker/${
+                        typeof row.slug === "object"
+                          ? row.slug.current
+                          : row.slug
+                      }`}
+                      underline="hover"
+                      variant="h4"
+                      sx={{ color: "#000" }}
+                    >
+                      {row.title}
+                    </Link>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography
+                      variant="h4"
                       sx={{
-                        backgroundColor: "#EFE9DA",
-                        padding: 2,
-                        marginBottom: 2,
+                        borderBottom: "1px solid #ccc",
+                        paddingBottom: 2,
                       }}
                     >
-                      <Link
-                        href={`/tracker/${
-                          typeof row.slug === "object"
-                            ? row.slug.current
-                            : row.slug
-                        }`}
-                        underline="hover"
-                        variant="h4"
-                        sx={{ color: "#000" }}
-                      >
-                        {row.title}
-                      </Link>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="h4"
-                        sx={{
-                          borderBottom: "1px solid #ccc",
-                          paddingBottom: 2,
-                        }}
-                      >
-                        Type
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="h4"
-                        sx={{
-                          borderBottom: "1px solid #ccc",
-                          fontWeight: "normal",
-                          paddingBottom: 2,
-                        }}
-                      >
-                        {row.type}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="h4"
-                        sx={{
-                          borderBottom: "1px solid #ccc",
-                          paddingBottom: 2,
-                        }}
-                      >
-                        Government
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="h4"
-                        sx={{
-                          borderBottom: "1px solid #ccc",
-                          fontWeight: "normal",
-                          paddingBottom: 2,
-                        }}
-                      >
-                        {row.country.displayTitle}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="h4"
-                        sx={{
-                          borderBottom: "1px solid #ccc",
-                          paddingBottom: 2,
-                        }}
-                      >
-                        Date Initiated
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="h4"
-                        sx={{
-                          borderBottom: "1px solid #ccc",
-                          fontWeight: "normal",
-                          paddingBottom: 2,
-                        }}
-                      >
-                        {moment(new Date(row.dateInitiated)).strftime(
-                          "%b %d, %Y"
-                        )}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="h4"
-                        sx={{
-                          borderBottom: "1px solid #ccc",
-                          paddingBottom: 2,
-                        }}
-                      >
-                        Status
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="h4"
-                        sx={{
-                          borderBottom: "1px solid #ccc",
-                          fontWeight: "normal",
-                          paddingBottom: 2,
-                        }}
-                      >
-                        {row.status}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="h4"
-                        sx={{
-                          borderBottom: "1px solid #ccc",
-                          paddingBottom: 2,
-                        }}
-                      >
-                        Last Updated
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={6}>
-                      <Typography
-                        variant="h4"
-                        sx={{
-                          borderBottom: "1px solid #ccc",
-                          fontWeight: "normal",
-                          paddingBottom: 2,
-                        }}
-                      >
-                        {moment(new Date(row.lastUpdate)).strftime("%b %d, %Y")}
-                      </Typography>
-                    </Grid>
+                      Type
+                    </Typography>
                   </Grid>
-                </Paper>
-              )
-            )
+                  <Grid item xs={6}>
+                    <Typography
+                      variant="h4"
+                      sx={{
+                        borderBottom: "1px solid #ccc",
+                        fontWeight: "normal",
+                        paddingBottom: 2,
+                      }}
+                    >
+                      {row.type}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography
+                      variant="h4"
+                      sx={{
+                        borderBottom: "1px solid #ccc",
+                        paddingBottom: 2,
+                      }}
+                    >
+                      Government
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography
+                      variant="h4"
+                      sx={{
+                        borderBottom: "1px solid #ccc",
+                        fontWeight: "normal",
+                        paddingBottom: 2,
+                      }}
+                    >
+                      {row.country.displayTitle}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography
+                      variant="h4"
+                      sx={{
+                        borderBottom: "1px solid #ccc",
+                        paddingBottom: 2,
+                      }}
+                    >
+                      Date Initiated
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography
+                      variant="h4"
+                      sx={{
+                        borderBottom: "1px solid #ccc",
+                        fontWeight: "normal",
+                        paddingBottom: 2,
+                      }}
+                    >
+                      {moment(new Date(row.dateInitiated)).strftime(
+                        "%b %d, %Y"
+                      )}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography
+                      variant="h4"
+                      sx={{
+                        borderBottom: "1px solid #ccc",
+                        paddingBottom: 2,
+                      }}
+                    >
+                      Status
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography
+                      variant="h4"
+                      sx={{
+                        borderBottom: "1px solid #ccc",
+                        fontWeight: "normal",
+                        paddingBottom: 2,
+                      }}
+                    >
+                      {row.status}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography
+                      variant="h4"
+                      sx={{
+                        borderBottom: "1px solid #ccc",
+                        paddingBottom: 2,
+                      }}
+                    >
+                      Last Updated
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography
+                      variant="h4"
+                      sx={{
+                        borderBottom: "1px solid #ccc",
+                        fontWeight: "normal",
+                        paddingBottom: 2,
+                      }}
+                    >
+                      {moment(new Date(row.lastUpdate)).strftime("%b %d, %Y")}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Paper>
+            ))
         ) : (
           <TableContainer sx={{ maxHeight: 440 }}>
             <Table
