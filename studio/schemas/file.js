be343c5a95aadc944bc1b7eb6982dc7aebe7928e
Harum Helmy <harumendhah@gmail.com>
2022-03-01 13:24:15 -0500

    Add ability to upload files and generate URLs

---
+++ studio/schemas/file.js
@@ -0,0 +1,46 @@
+import { tryGetFile } from "@sanity/asset-utils";
+
+export default {
+  type: "document",
+  name: "uploadedFile",
+  title: "File uploads",
+  preview: {
+    select: {
+      title: "title",
+    },
+  },
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      description: "File title",
+      type: "string",
+    },
+    {
+      name: "theFile",
+      title: "Upload file",
+      type: "file",
+    },
+    {
+      name: "generateUrl",
+      title: "Generate URL",
+      description: "Click generate to get link the file",
+      type: "slug",
+      options: {
+        // this source takes all data that is currently in this document and pass it as argument
+        // then tryGetFile() - getting file from sanity with all atributes like url, original name etc
+        source: ({ theFile }) => {
+          if (!theFile) return "Missing PDF File";
+          const { asset } = tryGetFile(theFile?.asset?._ref, {
+            dataset: process.env.SANITY_STUDIO_DATASET,
+            projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+          });
+          return asset?.url;
+        },
+        // this slugify prevent from changing "/" to "-" it keeps the original link and prevent from slugifying
+        slugify: (link) => link,
+      },
+      validation: (Rule) => [Rule.required()],
+    },
+  ],
+};
