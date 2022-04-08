e626e3e1ab59a9cc40f5603b497cfcf5d1b178cd
Harum Helmy <harumendhah@gmail.com>
2022-04-08 14:22:43 -0400

    Remove unnecessary property on the file uploads schema

--- studio/schemas/file.js
+++ studio/schemas/file.js
@@ -21,26 +21,5 @@ export default {
       title: "Upload file",
       type: "file",
     },
-    {
-      name: "generateUrl",
-      title: "Generate URL",
-      description: "Click generate to get link the file",
-      type: "slug",
-      options: {
-        // this source takes all data that is currently in this document and pass it as argument
-        // then tryGetFile() - getting file from sanity with all atributes like url, original name etc
-        source: ({ theFile }) => {
-          if (!theFile) return "Missing PDF File";
-          const { asset } = tryGetFile(theFile?.asset?._ref, {
-            dataset: "production",
-            projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
-          });
-          return asset?.url;
-        },
-        // this slugify prevent from changing "/" to "-" it keeps the original link and prevent from slugifying
-        slugify: (link) => link,
-      },
-      validation: (Rule) => [Rule.required()],
-    },
   ],
 };
