1092650c235ce71fb72b96e1a9349d1f6638d0dc
Josh King <jking@chambana.net>
2022-07-08 15:26:42 -0400

    Update github workflow to use Node v16.* and fixup lockfileVersion.

--- deploy/install.sh
+++ deploy/install.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y --no-install-recommends ca-certificates wget git
 
-wget -qO- https://deb.nodesource.com/setup_14.x | sudo -E bash -
+wget -qO- https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Update submodule if necessary.
