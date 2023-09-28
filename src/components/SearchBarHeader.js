cbf58fa3345bfcfc401f2bb030451f1d804edd7e
Rae Gaines <rae@throneless.tech>
2023-09-28 10:59:04 -0400

    hook up search bar in nav bar

--- src/components/SearchBarHeader.js
+++ src/components/SearchBarHeader.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 // Material-ui imports
+import { CircularProgress } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import Input from "@mui/material/Input";
 import Paper from "@mui/material/Paper";
@@ -11,50 +12,64 @@ import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState(false)
   const [value, setValue] = useState();
 
   const handleSearchRequest = (value) => {
-    console.log(value);
-    // const url = "/search?" + encodeURI(value);
-    // if (router && router.push) {
-    //   console.log(router);
-    // router.push(url);
-    // }
+    console.log(router);
+    const url = "/search?" + encodeURI(value);
+    if (router && router.push) {
+      console.log(router);
+      router.push(url);
+    }
   };
+  
+  useEffect(() => { 
+    setLoading(false); 
+    setValue('')
+  }, [router]);
 
-  useEffect(() => {}, [router]);
-
-  return (
-    <Paper
-      component="form"
-      elevation={0}
-      square
-      sx={{ border: "1px solid #000", borderRadius: "2px", maxWidth: 250 }}
-      onSubmit={(e) => e.preventDefault()}
-    >
-      <IconButton
-        type="search"
-        aria-label="search"
-        sx={{ width: 40 }}
-        onClick={handleSearchRequest(value)}
+  if (loading) {
+    return <CircularProgress />
+  } else {
+    return (
+      <Paper
+        component="form"
+        elevation={0}
+        square
+        sx={{ border: "1px solid #000", borderRadius: "2px", maxWidth: 250 }}
+        onSubmit={(e) => e.preventDefault()}
       >
-        <SearchIcon sx={{ color: "#000" }} />
-      </IconButton>
-      <Input
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        sx={{
-          fontFamily: "h1.fontFamily",
-          marginLeft: 1,
-          marginTop: 1,
-          "&:before": {
-            borderBottom: "none",
-          },
-        }}
-        placeholder="Search"
-      />
-    </Paper>
-  );
+        <IconButton
+          type="search"
+          aria-label="search"
+          sx={{ width: 40 }}
+        // onClick={handleSearchRequest(value)}
+        >
+          <SearchIcon sx={{ color: "#000" }} />
+        </IconButton>
+        <Input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleSearchRequest(value)
+              setLoading(true)
+            }
+          }}
+          sx={{
+            fontFamily: "h1.fontFamily",
+            marginLeft: 1,
+            marginTop: 1,
+            "&:before": {
+              borderBottom: "none",
+            },
+          }}
+          placeholder="Search"
+        />
+      </Paper>
+    );
+  }
 };
 
 export default SearchBar;
