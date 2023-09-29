c0a4487b2aeb96466d03aac018f9e7a080889da5
Rae Gaines <rae@throneless.tech>
2023-09-29 11:17:08 -0400

    fix validation error on section sign up

--- studio/schemas/section_sign_up.js
+++ studio/schemas/section_sign_up.js
@@ -2,4 +2,14 @@ export default {
   "type": "object",
   "name": "section_sign_up",
   "title": "Newsletter Sign Up Form",
+  "fields": [
+    {
+      type: 'string',
+      name: 'section_id',
+      title: 'Element ID',
+      description:
+        'A unique identifier that can be used when linking to this section, if any. Must not contain whitespace.',
+      validation: null,
+    },
+  ]
 }
