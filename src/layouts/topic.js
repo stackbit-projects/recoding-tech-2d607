b7fbad25b1e1be788e5068bcfc43ee90c9387d44
Harum <harum@throneless.tech>
2023-11-03 16:55:38 -0400

    Add subscribe box

--- src/layouts/topic.js
+++ src/layouts/topic.js
@@ -63,7 +63,76 @@ const Topic = (props) => {
               </Grid>
               <Grid spacing={4} direction="column" item sm={12} md={4}>
                 <Grid item sx={{ width: "100%" }}>
-                  Insert subscribe box here
+                  <div
+                    id="mlb2-5983225"
+                    className="ml-form-embedContainer ml-subscribe-form ml-subscribe-form-5983225"
+                  >
+                    <div className="ml-form-align-center">
+                      <div className="ml-form-embedWrapper embedForm">
+                        <div className="ml-form-embedBody ml-form-embedBodyDefault row-form">
+                          <div className="ml-form-embedContent">
+                            <h4>Our content. Delivered.</h4>
+                            <p style={{ textAlign: "center" }}>
+                              Subscribe for our weekly update on Government
+                              Policy, Tech News and Research
+                            </p>
+                          </div>
+                          <form
+                            className="ml-block-form"
+                            action="https://static.mailerlite.com/webforms/submit/s8h5n5"
+                            data-code="s8h5n5"
+                            method="post"
+                            target="_blank"
+                          >
+                            <div className="ml-form-formContent">
+                              <div className="ml-form-fieldRow ml-last-item">
+                                <div className="ml-field-group ml-field-email ml-validate-email ml-validate-required">
+                                  <input
+                                    aria-label="email"
+                                    aria-required="true"
+                                    type="email"
+                                    className="form-control"
+                                    data-inputmask=""
+                                    name="fields[email]"
+                                    placeholder="Email"
+                                    autoComplete="email"
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                            <input type="hidden" name="ml-submit" value="1" />
+                            <div className="ml-form-embedSubmit">
+                              <button type="submit" className="primary">
+                                Subscribe
+                              </button>
+                              <button
+                                disabled="disabled"
+                                style={{ display: "none" }}
+                                type="button"
+                                className="loading"
+                              >
+                                {" "}
+                                <div className="ml-form-embedSubmitLoad"></div>{" "}
+                                <span className="sr-only">Loading...</span>{" "}
+                              </button>
+                            </div>
+                            <input type="hidden" name="anticsrf" value="true" />
+                          </form>
+                        </div>
+                        <div
+                          className="ml-form-successBody row-success"
+                          style={{ display: "none" }}
+                        >
+                          <div className="ml-form-successContent">
+                            <h4>Thank you!</h4>
+                            <p style={{ textAlign: "center" }}>
+                              You have successfully joined our subscriber list.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </Grid>
               </Grid>
             </Grid>
