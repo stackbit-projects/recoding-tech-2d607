60a88dfe12a6f9149e36c170a86fc577fcb538dd
Harum <harum@throneless.tech>
2023-11-22 13:35:59 -0500

    Updates to next seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,10 @@ export async function getStaticProps() {
   console.log("Page [...slug].js getStaticProps, /");
   const [config] = await client.fetch(`*[_type == "config"]`);
   const topics = await client.fetch(
-    `*[_type == "topic"]{ displayName, link, slug, type }`
+    `*[_type == "topic" && stackbit_model_type == "page"]{ displayName, link, slug, type }`
   );
   const [page] = await client.fetch(
-    `*[_type == "advanced" && stackbit_url_path == "/"]{_id, _createdAt, title, sections[]{type, alsoFeatured[]->{title, author, category, date, type, slug, stackbit_model_type}, featuredArticle->{title, author, featuredImage, category, date, type, slug, stackbit_model_type}}}`
+    `*[_type == "advanced" && stackbit_url_path == "/"]{_id, _type, _createdAt, title, sections[]{type, alsoFeatured[]->{title, author, category, date, type, slug, stackbit_model_type}, featuredArticle->{title, author, featuredImage, category, date, type, slug, stackbit_model_type}}}`
   );
   return {
     props: { path: "/", page, data: { config, topics } },
