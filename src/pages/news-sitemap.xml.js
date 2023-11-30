8fa53490e1bbfb2a1fcf64de8559c4407d0b4f7e
Harum <harum@throneless.tech>
2023-11-30 10:14:43 -0500

    Add to news sitemap

--- src/pages/news-sitemap.xml.js
+++ src/pages/news-sitemap.xml.js
@@ -13,6 +13,8 @@ const getPosts = async () => {
 const generateNewsSitemap = (posts) => {
   const newsSitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
         xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
     ${posts
       .map((post) => {
