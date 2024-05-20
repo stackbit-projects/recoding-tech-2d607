7259778871f40a64868d0090c10254bc199ac5d9
Harum <harum@throneless.tech>
2024-05-20 15:13:48 -0400

    Fix linter errors

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -135,20 +135,20 @@ const Post = (props) => {
               </Typography>
               {breadcrumbs && breadcrumbs.length
                 ? breadcrumbs.map((crumb, index) => {
-                  if (index == breadcrumbs.length - 1) {
-                    return (
-                      <Typography key={crumb} variant="body2" color="#FF0033">
-                        {format(crumb)}
-                      </Typography>
-                    );
-                  } else {
-                    return (
-                      <Typography key={crumb} variant="body2" color="#FFF">
-                        {format(crumb)}
-                      </Typography>
-                    );
-                  }
-                })
+                    if (index == breadcrumbs.length - 1) {
+                      return (
+                        <Typography key={crumb} variant="body2" color="#FF0033">
+                          {format(crumb)}
+                        </Typography>
+                      );
+                    } else {
+                      return (
+                        <Typography key={crumb} variant="body2" color="#FFF">
+                          {format(crumb)}
+                        </Typography>
+                      );
+                    }
+                  })
                 : null}
             </Breadcrumbs>
           </Box>
