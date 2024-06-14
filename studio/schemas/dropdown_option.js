7b3e2e7c17e279d8c92d0da01f61a0b334ef6bbe
Harum <harum@throneless.tech>
2024-06-14 15:29:26 -0400

    Add new schema

---
+++ studio/schemas/dropdown_option.js
@@ -0,0 +1,41 @@
+export default {
+    type: 'document',
+    name: 'dropdown_option',
+    title: 'Dropdown option',
+    fields: [
+        {
+            title: 'Policy action dropdown options',
+            name: 'actionTypes',
+            type: 'object',
+            name: 'entry',
+            fields: [
+                {
+                    name: 'type',
+                    title: "Is this entry for a policy action's type or government?",
+                    type: 'string',
+                    "options": {
+                        "list": [
+                            { title: 'Type', value: 'actionType' },
+                            { title: 'Government', value: 'actionGovt' }
+                        ]
+                    },
+                    validation: (Rule) => Rule.required(),
+
+                },
+                {
+                    name: 'value',
+                    title: 'Enter the option value here',
+                    type: 'string',
+                    validation: (Rule) => Rule.required(),
+
+                }
+            ]
+        }
+    ],
+    preview: {
+        select: {
+            title: 'entry.value',
+        }
+    }
+}
+
