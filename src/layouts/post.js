4f88f0053fd0888c7c94f231d012bb4666a359f1
Harum <harum@throneless.tech>
2024-08-01 18:23:01 -0400

    Fix linter errors

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -244,7 +244,7 @@ const Post = (props) => {
                       mb={2}
                       flexDirection="column"
                       maxHeight={200}
-                    // maxWidth={'100%'}
+                      // maxWidth={'100%'}
                     >
                       <PortableText
                         value={page.toc}
