8f39da1c84f6dc6a1c35cda5355215b0c4ddb53b
Harum <harum@throneless.tech>
2024-08-08 14:51:11 -0400

    Add mailerlite popup

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 // base imports
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import _ from "lodash";
@@ -17,6 +17,16 @@ import { ThemeProvider } from "@mui/material/styles";
 
 const Body = (props) => {
   const { page, data, path } = props;
+
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.async = true;
+
+    script.innerHTML = `(function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){ var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);} f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i); var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000)); _.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml'); var ml_account = ml('accounts', '2358287', 'f5k1u1a9u6', 'load');`;
+
+    document.head.appendChild(script);
+  }, []);
 
   return (
     <>
@@ -56,21 +66,21 @@ const Body = (props) => {
           // let key_name = _.get(meta, "keyName", null) || "name";
           return _.get(meta, "relativeUrl", null) ? (
             _.get(props, "data.config.domain", null) &&
-              (() => {
-                let domain = _.trim(
-                  _.get(props, "data.config.domain", null),
-                  "/"
-                );
-                let rel_url = withPrefix(_.get(meta, "value", null));
-                let full_url = domain + rel_url;
-                return (
-                  <meta
-                    key={meta_idx}
-                    // {...attribute(key_name, _.get(meta, "name", null))}
-                    content={full_url}
-                  />
-                );
-              })()
+            (() => {
+              let domain = _.trim(
+                _.get(props, "data.config.domain", null),
+                "/"
+              );
+              let rel_url = withPrefix(_.get(meta, "value", null));
+              let full_url = domain + rel_url;
+              return (
+                <meta
+                  key={meta_idx}
+                  // {...attribute(key_name, _.get(meta, "name", null))}
+                  content={full_url}
+                />
+              );
+            })()
           ) : (
             <meta
               key={meta_idx + ".1"}
