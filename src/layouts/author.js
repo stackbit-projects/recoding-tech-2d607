ac79cce7861d93bf00fe86c42a360af599ded36c
Harum <harum@throneless.tech>
2024-08-09 16:46:29 -0400

    Linter fixes

--- src/layouts/author.js
+++ src/layouts/author.js
@@ -96,16 +96,16 @@ const Author = (props) => {
             >
               {topics.length
                 ? topics.map((topic) => (
-                  <Chip
-                    color="footer"
-                    key={topic._id}
-                    label={topic.displayName}
-                    sx={{
-                      fontWeight: 400,
-                      textTransform: "none",
-                    }}
-                  />
-                ))
+                    <Chip
+                      color="footer"
+                      key={topic._id}
+                      label={topic.displayName}
+                      sx={{
+                        fontWeight: 400,
+                        textTransform: "none",
+                      }}
+                    />
+                  ))
                 : null}
             </Stack>
             <Grid
@@ -118,43 +118,43 @@ const Author = (props) => {
             >
               {posts.length
                 ? posts.map((post) => (
-                  <Grid
-                    item
-                    key={post._id}
-                    sx={{
-                      marginTop: 2,
-                    }}
-                    xs={12}
-                    sm={5.5}
-                  >
-                    <Link
-                      href={`/${post.slug.current}`}
+                    <Grid
+                      item
+                      key={post._id}
                       sx={{
-                        borderBottom: "1px solid",
-                        borderBottomColor: "#EFE9DA",
-                        display: "block",
-                        paddingBottom: 2,
-                        textDecoration: "none !important",
+                        marginTop: 2,
                       }}
+                      xs={12}
+                      sm={5.5}
                     >
-                      <Typography
-                        component="div"
-                        variant="body1"
+                      <Link
+                        href={`/${post.slug.current}`}
                         sx={{
-                          color: "#000 !important",
-                          fontSize: "1em",
-                          fontWeight: "700",
-                          "&:hover": {
-                            color: "#225C9D !important",
-                            textDecoration: "none",
-                          },
+                          borderBottom: "1px solid",
+                          borderBottomColor: "#EFE9DA",
+                          display: "block",
+                          paddingBottom: 2,
+                          textDecoration: "none !important",
                         }}
                       >
-                        {post.title}
-                      </Typography>
-                    </Link>
-                  </Grid>
-                ))
+                        <Typography
+                          component="div"
+                          variant="body1"
+                          sx={{
+                            color: "#000 !important",
+                            fontSize: "1em",
+                            fontWeight: "700",
+                            "&:hover": {
+                              color: "#225C9D !important",
+                              textDecoration: "none",
+                            },
+                          }}
+                        >
+                          {post.title}
+                        </Typography>
+                      </Link>
+                    </Grid>
+                  ))
                 : null}
             </Grid>
           </Box>
