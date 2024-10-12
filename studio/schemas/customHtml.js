752e07011760188330cdf78c6ab3d5fe50068d4e
rich.jdev00 <rich.jdev00@gmail.com>
2024-10-12 22:47:51 +0400

    Add custom html support to Sentry Studio

---
+++ studio/schemas/customHtml.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'customHtml',
+  type: 'object',
+  title: 'Custom HTML code',
+  fields: [
+    {
+      name: 'code',
+      type: 'text',
+      title: 'Custom HTML code',
+    },
+  ]
+}
