d4a3fd8f25fb489ffb241e5d943c69f00738d9ba
rich.jdev00 <rich.jdev00@gmail.com>
2025-01-09 01:32:39 +0400

    Update Newsletter Subscribe button

--- src/components/NewsletterSubscribe.js
+++ src/components/NewsletterSubscribe.js
@@ -89,7 +89,7 @@ function NewsletterSubscribe() {
                         padding: "5px 10px !important",
                       }}
                     >
-                      SUBSCRIBE <ChevronRight />
+                      Subscribe <ChevronRight />
                     </button>
                     <button
                       disabled="disabled"
